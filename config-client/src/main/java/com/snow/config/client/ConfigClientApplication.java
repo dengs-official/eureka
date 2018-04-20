14278ecc3d450d509f79205f8e2335693dff137b
dengs.official <dengs.official@gmail.com>
2018-04-20 19:43:03 +0800

    update the config-client and config-server, update the spring-cloud-bus setting

--- config-client/src/main/java/com/snow/config/client/ConfigClientApplication.java
+++ config-client/src/main/java/com/snow/config/client/ConfigClientApplication.java
@@ -2,11 +2,19 @@ package com.snow.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigClientApplication.class, args);
+		// manually input target profiles
+		Scanner scan = new Scanner(System.in);
+		// input port of service
+		String port = scan.nextLine();
+		new SpringApplicationBuilder(ConfigClientApplication.class).properties("server.port="+port).run(args);
+		// SpringApplication.run(ConfigClientApplication.class, args);
 	}
 }
